144526fdfb8549012e90c537bdd2485d121be7fe
DariDel <darialber@gmail.com>
2026-03-10 22:32:56 -0500

    Se corrige el lenguaje y el time zone

--- config/settings/base.py
+++ config/settings/base.py
@@ -123,9 +123,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'es-co' #'en-us'
+LANGUAGE_CODE = 'es-co'
 
-TIME_ZONE = 'America/Bogota' #'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
